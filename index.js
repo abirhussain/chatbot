eb6ef6f20a84db16289c2cabd3343e956840e072
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2023-11-30 00:36:20 +0600

    created server and testing html file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
